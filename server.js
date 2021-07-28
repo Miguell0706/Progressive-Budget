d671de648d14606a5826d59468f23c2b6e21599a
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-28 15:12:33 -0700

    deploying

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
 const app = express();
 
 app.use(logger("dev"));
@@ -17,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
