546726a92a4f75cb67f026497fb1ef287fd4593b
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-28 15:13:24 -0700

    heroku deployment

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   }
 );
 
