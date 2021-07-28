936df5e70665819058fd769308e326d923ec9d0e
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-28 11:06:09 -0700

    commit

--- public/indexedData.js
+++ public/indexedData.js
@@ -1,51 +1,52 @@
-const indexDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || windows.shimIndexedDB;
-
+const indexDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 let db;
-
 const request = indexDB.open("budget", 1);
-
-request.onupgradeneeded = ({target})=>{
-    let db = target.result;
-    db.createObjectStore("pending", {autoIncrement: true});
-
-}
-
-request.onsucess = ({target})=> {
-    db =  target.result;
-
-    if(navigator.onLine){
-        checkDB();
-    }
-}
-request.onerror = function(e){
-    console.log("error" + e.target.errorCode);
-}
-
-function saveRecord(record){
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store= transaction.objectStore("pending");
-    store.add(record);
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
-
-function checkDB(){
-   const get =  db.transaction(["pending"], "readwrite").objectStore("pending").getAll()
-
-   get.onsuccess = function(){
-       if (get.result.length > 0){
-        fetch('/api/transaction/bulk', {
-            method: "POST", 
-            body: JSON.stringify(get.result),
-            headers: {
-                Accept:"application/json, text/plain, */*",
-                "Content-Type": "application/json"
-            }
-        }).then(res => {
-            return res.json();
-        }).then(()=>{
-            db.transaction(["pending"], "readwrite").objectStore('pending').clear()
-        })
-       }
-   }
+request.onsuccess = ({ target }) => {
+  db = target.result;
+  if (navigator.onLine) {
+    checkDB();
+  }
+};
+request.onerror = function (event) {
+  console.log("Error " + event.target.errorCode);
+};
+function saveRecord(record) {
+  const store = db.transaction(["pending"], "readwrite").objectStore("pending");
+  store.add(record);
 }
-
-window.addEventListener('online', checkDB)
+function checkDB() {
+  const get = db
+    .transaction(["pending"], "readwrite")
+    .objectStore("pending")
+    .getAll();
+  get.onsuccess = function () {
+    if (get.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(get.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then(() => {
+          db.transaction(["pending"], "readwrite")
+            .objectStore("pending")
+            .clear();
+        });
+    }
+  };
+}
+window.addEventListener("online", checkDB);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,7 @@
-const APP_PREFIX = 'my-system-cache-';
-const VERSION = 'v1';
+const APP_PREFIX = "my-site-cache-";
+const VERSION = "v1";
 const CACHE_name = APP_PREFIX + VERSION;
-const DATA_NAME = "data-cache-" + VERSION;
-
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
   "./styles.css",
@@ -12,67 +11,62 @@ const FILES_TO_CACHE = [
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
 ];
-
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches
-      .open(CACHE_name)
-      .then(function(data){
-          console.log("adding files: " + CACHE_name)
-          return data.addAll(FILES_TO_CACHE);
-        })
+    caches.open(CACHE_name).then(function (cache) {
+      console.log("installing cache : " + CACHE_name);
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      let keepList = cacheNames.filter(function (key) {
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let keeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
-
-      keepList.push(CACHE_name);
-
+      keeplist.push(CACHE_name);
       return Promise.all(
-        cacheNames.map((key, i) => {
-          if (keepList.indexOf(key) === -1) {
-            return caches.delete(cacheNames[i]);
+        keyList.map(function (key, i) {
+          if (keeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
           }
         })
       );
     })
   );
-});
-
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.include('/api/')) {
-      event.respondWith(
-        caches.open(DATA_NAME).then(data =>{
-            return fetch(event.request).then(response =>{
-                 if (response.status === 200) {
-             data.put(event.request.url, response.clone())
-          }
-          return response
-            }).catch( err => {
-                return data.match(event.request)
-            }).catch( err => console.log(err) )
-
-
-        })
-      );
-      return;
-    }
-
-    event.respondWith(
-        fetch(event.request).catch(function(){
-        return caches.match(event.request).then(function(res){
-            if (res){
-                return res
-            } else if(event.request.headers.get('accept').includes('text/html')){
-                return caches.match("/")
-            }
-        })
-    }))
-
-  });
-  
+}); 
