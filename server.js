cdf75c352ec487fa3f957cccf681487e1e1ac000
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-28 22:25:47 -0700

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,13 +18,17 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
